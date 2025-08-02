a8e1e93b013bdee0ea162eebdcc6eff40b77c81f
ambigg <imela2811@gmail.com>
2025-08-01 21:17:25 -0600

    building a test compiler

---
+++ cradle.h
@@ -0,0 +1,30 @@
+#ifndef CRADLE_H
+#define CRADLE_H
+
+#define UPCASE(C) ((1<<5) & (C))
+
+#define MAX_BUF 100
+char tmp[MAX_BUF];
+
+char Look;
+
+void GetChar();
+
+void Error(char *s);
+void Abort(char *s);
+void Expected(char *s);
+void Match(char x);
+
+int IsAlpha(char c);
+int IsDigit(char c);
+int IsAddop(char c);
+
+char GetName();
+char GetNum();
+
+void Emit(char *s);
+void EmitLn(char *s);
+
+void Init();
+
+#endif
